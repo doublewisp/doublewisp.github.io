b9949916874251704873c8d1bd4fdfa2885821ec
Mattia Rossi <mattia93@hotmail.com>
2025-12-05 14:32:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,166 @@
+//---------------------------------------//
+//---------VARIABLES AND CLASSES---------//
+
+const majorSize = 170;       //deck size for major arcana
+
+let majorDeck = [];
+
+    
+class arcaneMajor {             //arcane major card
+    constructor(index, name) {
+        this.index = index;         //storage index in sorted deck. Value is same as index 
+    }
+}    
+
+//--------------------------------//
+//---------INITIALIZATION---------//
+
+
+function createMajorDeck(){
+    for(i=0; i<majorSize; i++){
+        let newCard = new arcaneMajor(i, majorNames[i]);
+        majorDeck.push(newCard);
+    }
+}
+
+
+createMajorDeck();
+
+
+//-------------------------//
+//---------SHUFFLE---------//
+
+let majorCurrent = majorDeck.slice();
+
+//Fisher Yates Method to shuffle chosen deck
+function shuffleDeck(deckToShuffle){
+    for(i=deckToShuffle.length-1; i>0; i--) {
+        j = Math.floor(Math.random()*i);
+        k = deckToShuffle[i];
+        deckToShuffle[i] = deckToShuffle[j];
+        deckToShuffle[j] = k;
+    }
+}
+
+function init() {
+    shuffleDeck(majorCurrent);
+}
+
+init();
+
+//----------------------//
+//---------DRAW---------//
+
+let majorCardIndex = 0;
+
+//draw a card when button is pressed
+function drawCard(currentDeck, deckType) {
+    
+    if(deckType === 'major'){
+        document.getElementById('major_deck').src = "img/major/"+majorCurrent[majorCardIndex].index+".jpg";
+        document.getElementById('name_major').innerHTML= "<p>"+majorCurrent[majorCardIndex].name+"</p>";
+        if (majorCardIndex > majorSize-2) {      //shuffle if empty
+            shuffleDeck(majorCurrent);
+            majorCardIndex = 0;
+        }    
+        else {
+            hideBtn('shuffle_major');
+        }
+        majorCardIndex++;   
+    }
+}
+
+
+document.getElementById('draw_major').addEventListener('click', function(){
+    drawCard(majorCurrent, 'major');
+});
+
+
+//-------------------------//
+//---------BUTTONS---------//
+
+function showBtn(buttonID) {
+    document.getElementById(buttonID).style.display = 'block';
+}
+
+function hideBtn(buttonID) {
+    document.getElementById(buttonID).style.display = 'none';
+}
+
+document.getElementById('shuffle_major').addEventListener('click', function() {
+    shuffleDeck(majorCurrent);
+    majorCardIndex = 0;
+    hideBtn('shuffle_major');
+    document.getElementById('major_deck').src = "img/back.jpg";
+    document.getElementById('name_major').innerHTML= "<p></p>";
+});
+
+//-------------------------//
+//---------IMAGES----------//
+
+function imgTranslator(card){
+    let numeric, literal;
+    switch (card.value){
+            case 1:
+                numeric = 'A';
+                break;
+            case 11:
+                numeric = 'J';
+                break;
+            /*case 12:                
+                numeric = 'H';
+                break;*/
+            case 12:
+                numeric = 'Q';
+                break;
+            case 13:
+                numeric = 'K';
+                break;
+            default:
+                numeric = card.value;
+    }
+    switch (card.suitString){
+            case 'Hearts':
+                literal = 'H';
+                break;
+            case 'Diamonds':
+                literal = 'D';
+                break;
+            case 'Clubs':
+                literal = 'C';
+                break;
+            case 'Spades':
+                literal = 'S';
+                break;
+    }
+    return numeric + literal + '.png';
+}
+
+function valueTranslator(card) {
+    let literal;
+    switch (card.value){
+            case 1:
+                literal = 'Ace';
+                break;
+            case 11:
+                literal = 'Jack';
+                break;
+            /*case 12:                
+                literal = 'Knight';
+                break;*/
+            case 12:
+                literal = 'Queen';
+                break;
+            case 13:
+                literal = 'King';
+                break;
+            default:
+                literal = card.value;
+    }
+    return literal;
+}
+
+
+//console.log(majorDeck);
+//console.log(majorCurrent);
+//console.log(gameStarting);
