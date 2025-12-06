f88ceb1fe9eeff182d6b85c958c1f2025da68a40
Mattia Rossi <mattia93@hotmail.com>
2025-12-06 11:11:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,9 +19,10 @@
             document.getElementById("cardImage").src = "img/back.jpg";
         }
 
+        
         // Restituisce next card
         function getNextCard() {
-            if (CURRENT_CARD >= DECK.length) {
+            if (CURRENT_CARD >= DECK.length) { 
                 return null;
             }
             const filename = DECK[CURRENT_CARD] + ".jpg";
@@ -36,13 +37,41 @@
 
             if (card === null) {
                 // fine mazzo
+                shuffleDeck();
                 return;
             }
 
             const imgPath = "img/" + card;
             document.getElementById("cardImage").src = imgPath;
+            if (DECK[CURRENT_CARD-2]>0){
+                document.getElementById("past1").src = "img/" + DECK[CURRENT_CARD-2] + ".jpg";
+            }
+            if (DECK[CURRENT_CARD-3]>0){
+                document.getElementById("past2").src = "img/" + DECK[CURRENT_CARD-3] + ".jpg";
+            }
+            if (DECK[CURRENT_CARD-4]>0){
+                document.getElementById("past3").src = "img/" + DECK[CURRENT_CARD-4] + ".jpg";
+            }
+            if (DECK[CURRENT_CARD-5]>0){
+                document.getElementById("past4").src = "img/" + DECK[CURRENT_CARD-5] + ".jpg";
+            }
 
             firstClickDone = true;
+        }
+
+        // Ingrandisce carte passate
+        function setVisible(selected_card) {
+            document.getElementById("pastCard").src = document.getElementById(selected_card).src;
+            document.getElementById("pastCard").style.visibility = "visible";
+            document.getElementById("overlay").style.visibility = "visible";
+            document.getElementById("menu").style.visibility = "visible";
+        }
+
+        //hide all panels
+        function hideAll() {
+            document.getElementById("pastCard").style.visibility = "hidden";
+            document.getElementById("overlay").style.visibility = "hidden";
+            document.getElementById("menu").style.visibility = "hidden";
         }
 
         // Inizializzazione
